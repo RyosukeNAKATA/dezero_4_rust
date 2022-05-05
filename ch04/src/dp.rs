2c6a120b5ca5b3a4afda5f8dfce66ed67f92109f
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-05-06 00:40:56 +0900

    learn 4.1.2

--- ch04/src/dp.rs
+++ ch04/src/dp.rs
@@ -6,19 +6,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     V.insert("L1", 0f64);
     V.insert("L2", 0f64);
 
-    println!("V init {:?}", V);
-
-    for i in 0..100 {
+    let mut cnt = 0;
+    loop {
         let new_v = V.clone();
 
         let count_1 = V.entry("L1").or_insert(0.0);
         *count_1 = 0.5 * (-1 as f64 + 0.9 * new_v["L1"].clone())
             + 0.5 * (1 as f64 + 0.9 * new_v["L2"].clone());
+
+        let mut delta = (count_1.clone() - new_v["L1"].clone()).abs();
+
         let count_2 = V.entry("L2").or_insert(0.0);
         *count_2 = 0.5 * (0 as f64 + 0.9 * new_v["L1"].clone())
             + 0.5 * (-1 as f64 + 0.9 * new_v["L2"].clone());
 
-        println!("V {i} {:?}", new_v);
+        delta = delta.max((count_2.clone() - new_v["L2"].clone()).abs());
+
+        cnt += 1;
+        if delta < 0.0001 {
+            println!("V {:?}", V);
+            println!("cnt {:?}", cnt);
+            break;
+        }
     }
 
     Ok(())
